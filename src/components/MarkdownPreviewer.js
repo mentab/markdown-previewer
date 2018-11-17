38834e80e44841d9335bd40286992df819f4a140
mentab <25363363+mentab@users.noreply.github.com>
2018-11-17 15:37:22 +0100

    start working on tests

--- src/components/MarkdownPreviewer.js
+++ src/components/MarkdownPreviewer.js
@@ -7,13 +7,7 @@ import { Container, Row, Col } from 'reactstrap';
 class MarkdownPreviewer extends Component {
   constructor(props) {
     super(props);
-    const onloadStr = `
-# Markdown Previewer 
-
-## A markdown previewer
-
-[link to the exercise](https://learn.freecodecamp.org/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer/)
-
+    const onloadStr = `# Markdown Previewer\n## A markdown previewer\n[link to the exercise](https://learn.freecodecamp.org/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer/)\n
 \`\`\`javascript
 import marked from 'marked';
 getMarkdown(text) {
@@ -22,19 +16,9 @@ getMarkdown(text) {
   return htmlText;
 }
 \`\`\`
-
-* you can parse
-- you may parse
-+ you must parse
-
-> Parse it if you want it
-> If you don't want to parse, don't parse
-> If you want to parse, parse
-> If you don't know, or if you don't know what is parsing, parse
-
-![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Parse plz")
-
-Parse, **parse** or __parse__.
+* you can parse\n- you may parse\n+ you must parse\n
+> Parse it if you want it\n> If you don't want to parse, don't parse\n> If you want to parse, parse\n> If you don't know, or if you don't know what is parsing, parse\n
+![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Parse plz")\nParse, **parse** or __parse__.
 
 `;
     this.state = {
